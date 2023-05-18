f43912c963e272a421602a09a161963857966ee0
LCW640 <34577251+LCW640@users.noreply.github.com>
2023-05-18 23:58:29 +0800

    Add files via upload

---
+++ better-style.jsx
@@ -0,0 +1,13 @@
+const styleObj = {
+    color:red
+}
+
+function App() {
+  return (
+    <div className="App">
+      <div style={ styleObj }>this is a div</div>
+    </div>
+  )
+}
+
+export default App

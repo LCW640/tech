34385fce34322e39a588d04459d2d51f07ed6d67
LCW640 <34577251+LCW640@users.noreply.github.com>
2023-05-21 23:58:06 +0800

    Add files via upload

---
+++ 0521.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int storeWater(int[] bucket, int[] vat) {
+        int n = bucket.length;
+        int maxk = Arrays.stream(vat).max().getAsInt();
+        if (maxk == 0) {
+            return 0;
+        }
+        int res = Integer.MAX_VALUE;
+        for (int k = 1; k <= maxk && k < res; ++k) {
+            int t = 0;
+            for (int i = 0; i < bucket.length; ++i) {
+                t += Math.max(0, (vat[i] + k - 1) / k - bucket[i]);
+            }
+            res = Math.min(res, t + k);
+        }
+        return res;
+    }
+}

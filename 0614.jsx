3812ef17db7b59cfa675af181e7a5f79ad664f08
LCW640 <34577251+LCW640@users.noreply.github.com>
2023-06-14 23:35:15 +0800

    Add files via upload

---
+++ 0614.jsx
@@ -0,0 +1,22 @@
+class App extends React.Component {
+  state = {
+    message: 'this is message',
+  }
+  render() {
+    return (
+      <div>
+        <div>父组件</div>
+        <FSon
+          msg={this.state.messge}
+          age={20}
+          isMan={true}
+          cb={() => {
+            console.log(1)
+          }}
+          child={<span>this is child</span>}
+        />
+        <CSon msg={this.state.message} />
+      </div>
+    )
+  }
+}

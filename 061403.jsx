212681bc425d63ace79f34576107ba6ae708c834
LCW640 <34577251+LCW640@users.noreply.github.com>
2023-06-14 23:51:57 +0800

    Add files via upload

--- 061403.jsx
+++ 061403.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-// 子组件A
+//子组件a
 function SonA(props) {
   return (
     <div>
@@ -9,8 +9,8 @@ function SonA(props) {
     </div>
   )
 }
-// 子组件B
-function SonB(props) {
+//子组件b
+function Sonb(props) {
   return (
     <div>
       SonB
@@ -19,13 +19,12 @@ function SonB(props) {
   )
 }
 
-// 父组件
+//父组件
 class App extends React.Component {
-  // 父组件提供状态数据
+  //父组件提供状态数据
   state = {
     message: 'this is message',
   }
-  // 父组件提供修改数据的方法
   changeMsg = (newMsg) => {
     this.setState({
       message: newMsg,
@@ -38,7 +37,7 @@ class App extends React.Component {
         {/* 接收数据的组件 */}
         <SonA msg={this.state.message} />
         {/* 修改数据的组件 */}
-        <SonB changeMsg={this.changeMsg} />
+        <SonB msg={this.state.changeMsg} />
       </>
     )
   }

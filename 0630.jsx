f1f81fd2a090fbe6a90f2f744d5314ff00d9a829
LCW640 <34577251+LCW640@users.noreply.github.com>
2023-06-30 23:49:04 +0800

    Add files via upload

---
+++ 0630.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class InputComponent extends React.Component {
+  // 声明组件状态
+  state = {
+    message: 'this is message',
+  }
+  // 声明事件回调函数
+  changeHandler = (e) => {
+    this.setState({ message: e.target.value })
+  }
+  render() {
+    return (
+      <div>
+        {/* 绑定value 绑定事件*/}
+        <input value={this.state.message} onChange={this.changeHandler} />
+      </div>
+    )
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <InputComponent />
+    </div>
+  )
+}
+export default App

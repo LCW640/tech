ac12103a2257a5e8ff64a4aedeca8e6da817a091
LCW640 <34577251+LCW640@users.noreply.github.com>
2023-07-05 23:58:03 +0800

    Add files via upload

---
+++ 0705.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  // 参数：状态初始值比如,传入 0 表示该状态的初始值为 0
+  // 返回值：数组,包含两个值：1 状态值（state） 2 修改该状态的函数（setState）
+  const [count, setCount] = useState(0)
+  return (
+    <button
+      onClick={() => {
+        setCount(count + 1)
+      }}>
+      {count}
+    </button>
+  )
+}
+export default App
